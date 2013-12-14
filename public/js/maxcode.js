d4388b3375d35809d22b59c53ba117ae28180d38
Max <maxvharper@yahoo.com>
2013-12-14 16:12:59 -0700

    added row cb

--- public/js/maxcode.js
+++ public/js/maxcode.js
@@ -1,13 +1,17 @@
-var gridRunner = function (lat, lon, cb){
+var gridRunner = function (lat, lon, cb, rowcb){
     for (var i = 1; i <= lat; i++ ){
+        var myRow = [];
         for (var x = 1; x <= lon; x++ ){
             var item = cb(i,x);
             if(!item){
                 continue;
             }
             
+            myRow[x] = item;
             return item;
+        }
+        if(rowcb){
+            rowcb(myRow);
         }
     }
 };
-
