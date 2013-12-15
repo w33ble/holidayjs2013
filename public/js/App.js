d1fd40c9162bc948ab0c4ec067b23de994efb708
Joe Fleming <joe.fleming@gmail.com>
2013-12-14 17:56:33 -0700

    namespace games with uuid

--- public/js/App.js
+++ public/js/App.js
@@ -1,7 +1,9 @@
-App.Pieces = Backbone.Collection.extend({
+App.Pieces = Backbone.Firebase.Collection.extend({
   model: App.Piece,
-  firebase: 'https://holiday-js-hackathon-2013.firebaseio.com/',
+  // firebase: 'https://holiday-js-hackathon-2013.firebaseio.com/',
   initialize: function() {
+    var uuid4       = UUIDjs.create();
+    this.firebase = 'https://holiday-js-hackathon-2013.firebaseio.com/' + uuid4.toString();
     // this.listenTo('add', this, initPiece);
   }
 });;App.Piece = Backbone.Model.extend({
