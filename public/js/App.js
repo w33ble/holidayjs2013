8ee961f50288b90384a4f499e933eb65cef17ff0
Joe Fleming <joe.fleming@gmail.com>
2013-12-17 22:05:45 -0700

    put firebase url in a variable, add trailing slash to gameInstance

--- public/js/App.js
+++ public/js/App.js
@@ -44,7 +44,7 @@ App.Piece = Backbone.Model.extend({
   model: App.Piece,
   // firebase: 'https://holiday-js-hackathon-2013.firebaseio.com/',
   initialize: function() {
-    this.firebase = 'https://holiday-js-hackathon-2013.firebaseio.com/' + App.gameInstance;
+    this.firebase = App.firebaseUrl + App.gameInstance + 'pieces/';
   }
 });;/* TODO: move UUID code to main.js */
 App.Players = Backbone.Firebase.Collection.extend({
